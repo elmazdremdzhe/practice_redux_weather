74d14d89dc1cc8888132dd2787f1b6a987c4f266
Elmaz <dremdzhe@gmail.com>
2018-09-02 16:22:00 -0700

    created chart component from Sparklines and rendered temperatures chart using data from api

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,7 +1,25 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
+import Chart from '../components/chart';
 
 class WeatherList extends Component{
+
+    renderWeather(cityData){
+        const name= cityData.city.name;
+        const temps = cityData.list.map(weather => weather.main.temp);
+
+        console.log(temps);
+
+        return(
+            <tr key={name}>
+                <td>{name}</td>
+                <td>
+                    <Chart data={temps} color="orange"/>
+                </td>
+                
+            </tr>
+        );
+    }
 
     render(){
 
@@ -16,7 +34,7 @@ class WeatherList extends Component{
                 </tr>
                 </thead>
                 <tbody>
-
+                {this.props.weather.map(this.renderWeather)}
                 </tbody>
             </table>
         );
