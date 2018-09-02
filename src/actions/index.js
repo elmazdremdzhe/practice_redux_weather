dee0047aea2eb710a47c91b1d9b002064990914e
Elmaz <dremdzhe@gmail.com>
2018-09-01 17:52:48 -0700

    set up action creator FetchWeather that is responsible for getting data from Weather website api

---
+++ src/actions/index.js
@@ -0,0 +1,15 @@
+import axiosn from 'axios';
+
+const API_KEY='00ad5ec547e5b5033acc8aa8475aa874';
+const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city){
+    const url = `${ROOT_URL}&q=${city},us`;
+    const request = axios.get(url);
+
+    return{
+        type: FETCH_WEATHER,
+        payload: request
+    };
+}
