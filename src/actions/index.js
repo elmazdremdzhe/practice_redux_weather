e19b5eeecfa4761295d7480e7bb7835ccd7e488c
Elmaz <dremdzhe@gmail.com>
2018-09-02 10:24:49 -0700

    impemented reducer in responce to promise form api

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,14 @@
-import axiosn from 'axios';
+import axios from 'axios';
 
 const API_KEY='00ad5ec547e5b5033acc8aa8475aa874';
-const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city){
     const url = `${ROOT_URL}&q=${city},us`;
     const request = axios.get(url);
+
+    connsole.log('Request', request);
 
     return{
         type: FETCH_WEATHER,
