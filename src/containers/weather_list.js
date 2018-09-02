75555f08e8629225adf6bd15ed481dff77dac2d3
Elmaz <dremdzhe@gmail.com>
2018-09-02 11:37:22 -0700

    built weather table

---
+++ src/containers/weather_list.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import {connect} from 'react-redux';
+
+class WeatherList extends Component{
+
+    render(){
+
+        return(
+            <table className="table table-hover">
+                <thead>
+                <tr>
+                    <th>City</th>
+                    <th>Temperature</th>
+                    <th>Pressure</th>
+                    <th>Humidity</th>
+                </tr>
+                </thead>
+                <tbody>
+
+                </tbody>
+            </table>
+        );
+    }
+
+
+}
+
+//function mapStateToProps(state){
+//  const weather = state.weather
+// is identical to
+function mapStateToProps({weather}){
+    return { weather: weather };
+}
+
+export default connect(mapStateToProps)(WeatherList)
